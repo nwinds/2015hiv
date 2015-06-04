0324cb2d7458577ae8388302f16e7149e2a21d39
nwinds <icyplayer@163.com>
2015-06-04 15:07:11 +0800

    CRUD product(simple) ok

--- db/migrate/20150604065651_create_products.rb
+++ db/migrate/20150604065651_create_products.rb
@@ -1,12 +1,20 @@
+# coding: utf-8
 class CreateProducts < ActiveRecord::Migration
-  def change
+  def self.up
+    if table_exists?(:products)
+      drop_table :products
+    end
     create_table :products do |t|
       t.string :name
       t.text :detail
       t.string :icon_url
-      t.decimal :price
+      t.decimal :price, :precision => 8, :scale =>2
 
       t.timestamps null: false
     end
   end
+  # drop everytime it start agian
+  def self.down
+  	drop_table :products
+  end  	
 end
