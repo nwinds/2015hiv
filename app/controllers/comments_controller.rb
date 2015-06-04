d2c15c851fb2e9806ac1b1279c218760aeb1e5d4
nwinds <icyplayer@163.com>
2015-06-05 00:28:17 +0800

    littel adjust on submit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,7 +24,7 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
+    @comment = Comment.create(comment_params)
 
     respond_to do |format|
       if @comment.save
