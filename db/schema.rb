b6166c9d6a20dae6e5720c7d0e311829e02c508a
nwinds <namingwinds@gmail.com>
2015-06-06 15:37:24 +0800

    add wishlist

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150604115114) do
+ActiveRecord::Schema.define(version: 20150606072652) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter",  limit: 255
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 20150604115114) do
     t.decimal  "price",                    precision: 8, scale: 2
     t.datetime "created_at",                                       null: false
     t.datetime "updated_at",                                       null: false
+  end
+
+  create_table "wishlists", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
