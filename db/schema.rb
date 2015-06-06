1788edf21324b5aea7826efa80646e2fbb378c3c
nwinds <namingwinds@gmail.com>
2015-06-06 22:13:04 +0800

    wishlist same items combination: migration and rollback both available

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606133916) do
+ActiveRecord::Schema.define(version: 20150606135505) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter",  limit: 255
