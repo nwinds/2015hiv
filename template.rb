cb59d2c5349b8b6ac60826261a5a6d401fae6f43
nwinds <namingwinds@gmail.com>
2015-06-06 12:49:37 +0800

    add cmd template.rb in case forgoten

---
+++ template.rb
@@ -0,0 +1,12 @@
+# template.rb
+generate(:scaffold, "person name:string")
+route "root to: 'people#index'"
+rake("db:migrate")
+rake('test')
+rake('test:units')
+
+after_bundle do
+  git :init
+  git add: "."
+  git commit: %Q{ -m 'Initial commit' }
+end
