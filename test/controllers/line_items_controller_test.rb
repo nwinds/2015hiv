f1c35a13ef48cdce179f8705d64c5b706beb59ad
nwinds <namingwinds@gmail.com>
2015-06-10 13:23:34 +0800

    passed create test in line_items_controller_test

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -2,6 +2,9 @@ require 'test_helper'
 
 class LineItemsControllerTest < ActionController::TestCase
   setup do
+    # unquote this line to prepare data, (surely every tests here will report error), so that the data in the .yml will be put into the test database
+    # then quote this line, rerun the test
+    # prepare_data_for_wishlist()
     @line_item = line_items(:one)
     @update = line_items(:two)
   end
@@ -18,7 +21,11 @@ class LineItemsControllerTest < ActionController::TestCase
   end
 
   test "should create line_item" do
-    product = products(:ruby)
+    # use a stupid 1:run 2:quote way to prepare data
+    # Product.where(:product_id => 4).destroy
+    # Product.new(products(:LineItemProduct_four))
+
+    product = products(:LineItemProduct_four)
     assert_difference('LineItem.count') do
       post :create, :product_id => product
     end
@@ -49,4 +56,21 @@ class LineItemsControllerTest < ActionController::TestCase
 
     assert_redirected_to products_url
   end
+
+private
+  # data preparation: for the very three:
+  #   test between products, line_items, wishlist
+  def prepare_data_for_wishlist()
+    LineItem.new(line_items(:one))
+    LineItem.new(line_items(:two))
+    LineItem.new(line_items(:three))
+    LineItem.new(line_items(:four))
+    LineItem.new(line_items(:five))
+    LineItem.new(line_items(:six))
+  end
+
+
+
+
 end
+
