eb957427a36c6bc31311c0d27eab37735192bca5
nwinds <namingwinds@gmail.com>
2015-06-10 17:33:01 +0800

    test:functionals on line_items 2P1F

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -9,46 +9,49 @@ class LineItemsControllerTest < ActionController::TestCase
     @update = line_items(:two)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:line_items)
-  end
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
+  #   assert_not_nil assigns(:line_items)
+  # end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get :new
+  #   assert_response :success
+  # end
 
   test "should create line_item" do
-    # use a stupid 1:run 2:quote way to prepare data
-    # Product.where(:product_id => 4).destroy
-    # Product.new(products(:LineItemProduct_four))
-    product = products(:LineItemProduct_four)
+  #   # use a stupid 1:run 2:quote way to prepare data
+  #   # Product.where(:product_id => 4).destroy
+  #   # Product.new(products(:LineItemProduct_four))
+    product = products(:one)
     assert_difference('LineItem.count') do
-      post :create, :product_id => product
+      post :create, :product_id => product, :id => @line_item
     end
 
     assert_redirected_to products_url
   end
 
-  test "should show line_item" do
-    get :show, id: @line_item
-    assert_response :success
+  # test "should show line_item" do
+  #   get :show, id: @line_item
+  #   assert_response :success
+  # end
+
+  test "should get minus down" do
+    post :minus_down, id: @line_item, line_item: @update.to_param, :product_id => products(:one)
+    assert_redirected_to products_url
   end
 
-  test "should get edit" do
-    get :edit, id: @line_item, line_item: @update.to_param, product_id: @update.product_id.to_param
-    assert_response :success
-  end
-
-  test "should update line_item" do
-    patch :update, id: @line_item, line_item: @update.to_param, product_id: @update.product_id.to_param, wishlist_id: @update.wishlist_id.to_param
-    assert_redirected_to line_item_path(assigns(:line_item))
-  end
+  # test "should update line_item" do
+  #   patch :update, id: @line_item, line_item: @update.to_param, product_id: @update.product_id.to_param, wishlist_id: @update.wishlist_id.to_param
+  #   assert_redirected_to line_item_path(assigns(:line_item))
+  # end
 
   test "should destroy line_item" do
     # test case is not good, line_item with the product id does not exists
+    product = products(:one)
+
+    # the line_item will be deleted
     assert_difference('LineItem.count', -1) do
       delete :destroy, id: @line_item, product_id: @line_item.product_id.to_param, wishlist_id: @line_item.wishlist_id
     end
